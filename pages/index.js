c7cc494f8901dc41f32b28a3a54c8fe4509530ea
Matheus Pedro <matheus.pedro1214@gmail.com>
2021-01-18 19:49:42 -0300

    Nova home

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,6 @@
-import { useState } from "react";
-
 function Home() {
     return (
-        <div>
-            <div>Home</div>
-            <Contador />
-        </div>
-    )
-}
-
-function Contador() {
-    const [contador, setContador] = useState(1);
-
-    function adcionarContador() {
-        setContador(contador + 2);
-    }
-
-    return (
-        <div>
-            <div>{contador}</div>
-            <button onClick={adcionarContador}>Adicionar</button>
-        </div>
+        <div>Home</div>
     )
 }
 
